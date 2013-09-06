0605f6eee1c684f04108a749ad992603b768eae5
maxsuhak <maxsuhak@gmail.com>
2013-09-06 17:43:24 +0300

    onfig.action_mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Attestation::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost' }
 end
