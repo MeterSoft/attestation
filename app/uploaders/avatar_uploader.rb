ccdea7ca00749eebda919b244f47ccd444d07e64
maxsuhak <maxsuhak@gmail.com>
2013-09-10 18:22:47 +0300

    added jquery-ui

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,4 +15,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   version :thumb do
     process :resize_to_fill => [200,200]
   end
+  version :mini do
+    process :resize_to_fill => [50,50]
+  end
 end
