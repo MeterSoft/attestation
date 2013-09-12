dcc78c5afb0db0661fbf5182db997de8a734ca35
Mikhail Nikalyukin <m.nikalyukin@svitla.com>
2013-09-12 13:44:08 +0300

    added new design to task form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
-	def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+	def link_to_remove_fields(name, f, klass = nil, title = nil)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", class: klass, title: title)
   end
   
-  def link_to_add_fields(name, f, association)
+  def link_to_add_fields(name, f, association, klass = nil, title = nil)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", class: klass, title: title)
   end
 
   def admin?
